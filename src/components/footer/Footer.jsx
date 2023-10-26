ed5c7235ccd76ebc839cd8f173cf1e682495a912
dodam24 <dodam0724@gmail.com>
2023-10-26 20:37:07 +0900

    Search Result List Page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,7 +12,6 @@ const Footer = () => {
           <li className="fListItem">Airports</li>
           <li className="fListItem">Hotels</li>
         </ul>
-
         <ul className="fList">
           <li className="fListItem">Homes</li>
           <li className="fListItem">Apartments</li>
@@ -21,7 +20,6 @@ const Footer = () => {
           <li className="fListItem">Hostels</li>
           <li className="fListItem">Guest houses</li>
         </ul>
-
         <ul className="fList">
           <li className="fListItem">Unique places to stay</li>
           <li className="fListItem">Reviews</li>
@@ -29,14 +27,12 @@ const Footer = () => {
           <li className="fListItem">Travel communities</li>
           <li className="fListItem">Seasonal and holiday deals</li>
         </ul>
-
         <ul className="fList">
           <li className="fListItem">Car rental</li>
           <li className="fListItem">Flight Finder</li>
           <li className="fListItem">Restaurant reservations</li>
           <li className="fListItem">Travel Agents</li>
         </ul>
-
         <ul className="fList">
           <li className="fListItem">Customer Service</li>
           <li className="fListItem">Partner Help</li>
