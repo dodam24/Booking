a4620251181d626d40c6746e94fbd97bf86418a7
dodam24 <dodam0724@gmail.com>
2023-10-31 19:58:47 +0900

    JWT(JSON Web Token) with Cookie, Authorization

--- bookingApp/api/index.js
+++ bookingApp/api/index.js
@@ -5,6 +5,8 @@ import authRoute from "./routes/auth.js";
 import usersRoute from "./routes/users.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+
 const app = express();
 dotenv.config();
 
@@ -22,6 +24,7 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // middlewares
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
@@ -30,15 +33,15 @@ app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
 app.use((err, req, res, next) => {
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || "Something went wrong!"
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
     message: errorMessage,
     stack: err.stack,
-  })
-})
+  });
+});
 
 app.listen(8000, () => {
   connect();
